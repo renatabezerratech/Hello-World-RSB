e285341daedf32730cc74bcad5b9f2fc707d516d
Renata Bezerra <renatabezerratecnologia@gmail.com>
2021-07-14 22:12:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello-World-RSB
+This is my first project, it was produced in HTML5, CSS3 and the Adobe Photoshop.
---
+++ Site/script.js
@@ -0,0 +1,4 @@
+    function clicou(){
+    var nome = window.prompt('Qual é o seu nome?');
+    window.alert(`Olá ${nome}, que bom te ver por aqui!`);
+    }
